c1c837de4ec00e700222c7a394fa37b7e767a783
Kasturi-Sasank <kasturisasank07@gmail.com>
2025-09-25 19:27:27 +0530

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ docker pull ${DOCKER_HUB_USERNAME}/savenserve-frontend:latest
 docker pull ${DOCKER_HUB_USERNAME}/savenserve-backend:latest
 
 # Stop and remove existing containers
-docker-compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml down
 
 # Start the containers with the new images
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d
 
 # Clean up unused images
 docker image prune -f
